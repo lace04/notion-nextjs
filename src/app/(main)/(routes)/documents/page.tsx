5f7cd7440b18b0af4f6da0066457a3ec040ab537
lace04 <luisace04@gmail.com>
2023-10-13 15:52:38 -0500

    Schema & API

--- src/app/(main)/(routes)/documents/page.tsx
+++ src/app/(main)/(routes)/documents/page.tsx
@@ -2,11 +2,26 @@
 
 import Image from 'next/image';
 import { useUser } from '@clerk/clerk-react';
-import { Button } from '@/components/ui/button';
 import { PlusCircle } from 'lucide-react';
+import { useMutation } from 'convex/react';
+import { toast } from 'sonner';
+import { api } from '../../../../../convex/_generated/api';
+import { Button } from '@/components/ui/button';
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: 'Untitled' });
+
+    toast.promise(promise, {
+      loading: 'Creating a new note...',
+      success: 'Note created!',
+      error: 'Something went wrong!',
+    });
+  };
+
   return (
     <div className='h-full flex flex-col items-center justify-center space-y-4'>
       <Image
@@ -26,7 +41,7 @@ const DocumentsPage = () => {
       <h2 className='text-lg font-medium'>
         Welcome to {user?.firstName}&apos;s Notion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className='w-4 h-4 mr-2' />
         Create a note
       </Button>
