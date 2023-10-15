d30c7d6193ad346e59b2e5847c4c9837f8ed206e
lace04 <luisace04@gmail.com>
2023-10-15 16:32:33 -0500

    Trash box

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,4 +1,5 @@
 import { v } from 'convex/values';
+
 import { mutation, query } from './_generated/server';
 import { Doc, Id } from './_generated/dataModel';
 
@@ -6,14 +7,22 @@ export const archive = mutation({
   args: { id: v.id('documents') },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
-    if (!identity) throw new Error('Not logged in');
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
     const userId = identity.subject;
 
     const existingDocument = await ctx.db.get(args.id);
 
-    if (!existingDocument) throw new Error('Document not found');
+    if (!existingDocument) {
+      throw new Error('Not found');
+    }
 
-    if (existingDocument.userId !== userId) throw new Error('Unauthorized');
+    if (existingDocument.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
 
     const recursiveArchive = async (documentId: Id<'documents'>) => {
       const children = await ctx.db
@@ -36,7 +45,7 @@ export const archive = mutation({
       isArchived: true,
     });
 
-    await recursiveArchive(args.id);
+    recursiveArchive(args.id);
 
     return document;
   },
@@ -48,30 +57,21 @@ export const getSidebar = query({
   },
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
-    if (!identity) throw new Error('Not logged in');
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
     const userId = identity.subject;
 
     const documents = await ctx.db
       .query('documents')
-      .withIndex(
-        'by_user_parent',
-        (q) => q.eq('userId', userId).eq('parentDocument', args.parentDocument) //Esto es para que solo me traiga los documentos que tengan como parentDocument el que le estoy pasando
+      .withIndex('by_user_parent', (q) =>
+        q.eq('userId', userId).eq('parentDocument', args.parentDocument)
       )
-      .filter((q) => q.eq(q.field('isArchived'), false)) //Esto es para que me traiga los documentos que no esten archivados
-      .order('desc') //Esto es para que me traiga los documentos que no esten archivados
-      .collect(); //Esto es para que me traiga los documentos que no esten archivados
-
-    return documents;
-  },
-});
-
-export const get = query({
-  handler: async (ctx) => {
-    const identity = await ctx.auth.getUserIdentity();
-
-    if (!identity) throw new Error('Not logged in');
-
-    const documents = await ctx.db.query('documents').collect();
+      .filter((q) => q.eq(q.field('isArchived'), false))
+      .order('desc')
+      .collect();
 
     return documents;
   },
@@ -85,17 +85,271 @@ export const create = mutation({
   handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
 
-    if (!identity) throw new Error('Not logged in');
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
 
     const userId = identity.subject;
 
     const document = await ctx.db.insert('documents', {
       title: args.title,
-      userId,
       parentDocument: args.parentDocument,
+      userId,
       isArchived: false,
       isPublished: false,
     });
+
+    return document;
+  },
+});
+
+export const getTrash = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query('documents')
+      .withIndex('by_user', (q) => q.eq('userId', userId))
+      .filter((q) => q.eq(q.field('isArchived'), true))
+      .order('desc')
+      .collect();
+
+    return documents;
+  },
+});
+
+export const restore = mutation({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error('Not found');
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const recursiveRestore = async (documentId: Id<'documents'>) => {
+      const children = await ctx.db
+        .query('documents')
+        .withIndex('by_user_parent', (q) =>
+          q.eq('userId', userId).eq('parentDocument', documentId)
+        )
+        .collect();
+
+      for (const child of children) {
+        await ctx.db.patch(child._id, {
+          isArchived: false,
+        });
+
+        await recursiveRestore(child._id);
+      }
+    };
+
+    const options: Partial<Doc<'documents'>> = {
+      isArchived: false,
+    };
+
+    if (existingDocument.parentDocument) {
+      const parent = await ctx.db.get(existingDocument.parentDocument);
+      if (parent?.isArchived) {
+        options.parentDocument = undefined;
+      }
+    }
+
+    const document = await ctx.db.patch(args.id, options);
+
+    recursiveRestore(args.id);
+
+    return document;
+  },
+});
+
+export const remove = mutation({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error('Not found');
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const document = await ctx.db.delete(args.id);
+
+    return document;
+  },
+});
+
+export const getSearch = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query('documents')
+      .withIndex('by_user', (q) => q.eq('userId', userId))
+      .filter((q) => q.eq(q.field('isArchived'), false))
+      .order('desc')
+      .collect();
+
+    return documents;
+  },
+});
+
+export const getById = query({
+  args: { documentId: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    const document = await ctx.db.get(args.documentId);
+
+    if (!document) {
+      throw new Error('Not found');
+    }
+
+    if (document.isPublished && !document.isArchived) {
+      return document;
+    }
+
+    if (!identity) {
+      throw new Error('Not authenticated');
+    }
+
+    const userId = identity.subject;
+
+    if (document.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+
+    return document;
+  },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id('documents'),
+    title: v.optional(v.string()),
+    content: v.optional(v.string()),
+    coverImage: v.optional(v.string()),
+    icon: v.optional(v.string()),
+    isPublished: v.optional(v.boolean()),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Unauthenticated');
+    }
+
+    const userId = identity.subject;
+
+    const { id, ...rest } = args;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error('Not found');
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      ...rest,
+    });
+
+    return document;
+  },
+});
+
+export const removeIcon = mutation({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Unauthenticated');
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error('Not found');
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      icon: undefined,
+    });
+
+    return document;
+  },
+});
+
+export const removeCoverImage = mutation({
+  args: { id: v.id('documents') },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error('Unauthenticated');
+    }
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+
+    if (!existingDocument) {
+      throw new Error('Not found');
+    }
+
+    if (existingDocument.userId !== userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const document = await ctx.db.patch(args.id, {
+      coverImage: undefined,
+    });
+
     return document;
   },
 });
--- src/app/(main)/_components/navigation.tsx
+++ src/app/(main)/_components/navigation.tsx
@@ -1,24 +1,41 @@
 'use client';
 
-import { cn } from '@/lib/utils';
 import {
   ChevronsLeft,
   MenuIcon,
+  Plus,
   PlusCircle,
   Search,
   Settings,
+  Trash,
 } from 'lucide-react';
-import { usePathname } from 'next/navigation';
+import { useParams, usePathname, useRouter } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
-import { UserItem } from './user-item';
 import { useMutation } from 'convex/react';
-import { api } from '../../../../convex/_generated/api';
-import { Item } from './item';
 import { toast } from 'sonner';
+
+import { cn } from '@/lib/utils';
+import { api } from '../../../../convex/_generated/api';
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@/components/ui/popover';
+import { useSearch } from '@/hooks/use-search';
+import { useSettings } from '@/hooks/use-settings';
+
+import { UserItem } from './user-item';
+import { Item } from './item';
 import { DocumentList } from './document-list';
+import { TrashBox } from './trash-box';
+import { Navbar } from './navbar';
 
 export const Navigation = () => {
+  const router = useRouter();
+  const settings = useSettings();
+  const search = useSearch();
+  const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
   const create = useMutation(api.documents.create);
@@ -104,13 +121,15 @@ export const Navigation = () => {
     }
   };
 
-  const handleCreate = async () => {
-    const promise = create({ title: 'Untitled' });
+  const handleCreate = () => {
+    const promise = create({ title: 'Untitled' }).then((documentId) =>
+      router.push(`/documents/${documentId}`)
+    );
 
     toast.promise(promise, {
       loading: 'Creating a new note...',
-      success: 'Note created!',
-      error: 'Failed to create note',
+      success: 'New note created!',
+      error: 'Failed to create a new note.',
     });
   };
 
@@ -134,14 +153,26 @@ export const Navigation = () => {
         >
           <ChevronsLeft className='h-6 w-6' />
         </div>
-        <div className=''>
+        <div>
           <UserItem />
-          <Item isSearch onClick={() => {}} label='Search' icon={Search} />
-          <Item onClick={() => {}} label='Settings' icon={Settings} />
-          <Item onClick={handleCreate} label='New Page' icon={PlusCircle} />
+          <Item label='Search' icon={Search} isSearch onClick={search.onOpen} />
+          <Item label='Settings' icon={Settings} onClick={settings.onOpen} />
+          <Item onClick={handleCreate} label='New page' icon={PlusCircle} />
         </div>
         <div className='mt-4'>
           <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label='Add a page' />
+          <Popover>
+            <PopoverTrigger className='w-full mt-4'>
+              <Item label='Trash' icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent
+              className='p-0 w-72'
+              side={isMobile ? 'bottom' : 'right'}
+            >
+              <TrashBox />
+            </PopoverContent>
+          </Popover>
         </div>
         <div
           onMouseDown={handleMouseDown}
@@ -157,15 +188,19 @@ export const Navigation = () => {
           isMobile && 'left-0 w-full'
         )}
       >
-        <nav className='bg-transparent px-3 py-2 w-full'>
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              role='button'
-              className='h-6 w-6 text-muted-foreground'
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className='bg-transparent px-3 py-2 w-full'>
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                role='button'
+                className='h-6 w-6 text-muted-foreground'
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
