43d7e93b894170ba719c3dcbe9ac9a510d11576c
lace04 <luisace04@gmail.com>
2023-10-18 12:39:16 -0500

    Advanved file upload (delete and replace)

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -10,6 +10,7 @@ import { useCoverImage } from '@/hooks/use-cover-image';
 import { api } from '../../convex/_generated/api';
 import { useMutation } from 'convex/react';
 import { Id } from '../../convex/_generated/dataModel';
+import { useEdgeStore } from '@/lib/edgestore';
 
 interface CoverImageProps {
   url?: string;
@@ -17,11 +18,17 @@ interface CoverImageProps {
 }
 
 export const Cover = ({ url, preview }: CoverImageProps) => {
+  const { edgestore } = useEdgeStore();
   const params = useParams();
   const coverImage = useCoverImage();
   const removeCoverImage = useMutation(api.documents.removeCoverImage);
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url,
+      });
+    }
     removeCoverImage({
       id: params.documentId as Id<'documents'>,
     });
@@ -41,7 +48,7 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
       {url && !preview && (
         <div className='opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2'>
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className='text-muted-foreground text-xs'
             variant='outline'
             size='sm'
